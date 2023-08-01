e38bf078063b835bbacaa23966ae1f5d871e4d6d
greenjerry <admin@xdrv.cn>
2023-08-01 19:25:16 +0800

    允许指定apiversion，但是默认2023-03-15-preview，2023-07-01-preview可能存在问题

--- HttpTrigger1/app.py
+++ HttpTrigger1/app.py
@@ -58,7 +58,7 @@ AZURE_OPENAI_STOP_SEQUENCE = os.environ.get("AZURE_OPENAI_STOP_SEQUENCE")
 AZURE_OPENAI_SYSTEM_MESSAGE = os.environ.get(
     "AZURE_OPENAI_SYSTEM_MESSAGE", "You are an AI assistant that helps people find information.")
 AZURE_OPENAI_PREVIEW_API_VERSION = os.environ.get(
-    "AZURE_OPENAI_PREVIEW_API_VERSION", "2023-06-01-preview")
+    "AZURE_OPENAI_PREVIEW_API_VERSION", "2023-03-15-preview")
 AZURE_OPENAI_STREAM = os.environ.get("AZURE_OPENAI_STREAM", "true")
 # Name of the model, e.g. 'gpt-35-turbo' or 'gpt-4'
 AZURE_OPENAI_MODEL_NAME = os.environ.get(
@@ -114,9 +114,9 @@ def prepare_body_headers_with_data(request):
 
     chatgpt_url = f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/openai/deployments/{AZURE_OPENAI_MODEL}"
     if is_chat_model():
-        chatgpt_url += "/chat/completions?api-version=2023-03-15-preview"
+        chatgpt_url += f"/chat/completions?api-version={AZURE_OPENAI_PREVIEW_API_VERSION}"
     else:
-        chatgpt_url += "/completions?api-version=2023-03-15-preview"
+        chatgpt_url += f"/completions?api-version={AZURE_OPENAI_PREVIEW_API_VERSION}"
 
     headers = {
         'Content-Type': 'application/json',
@@ -215,7 +215,7 @@ def stream_without_data(response):
 def conversation_without_data(request):
     openai.api_type = "azure"
     openai.api_base = f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/"
-    openai.api_version = "2023-03-15-preview"
+    openai.api_version = f"{AZURE_OPENAI_PREVIEW_API_VERSION}"
     openai.api_key = AZURE_OPENAI_KEY
 
     request_messages = request.json["messages"]
